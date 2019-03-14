0c85e8407fe9b7403f226ce2a1bbe0425c239190
andresshare <andresshare@gmail.com>
2019-03-14 10:50:55 -0500

    models views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from home.models import Contacto
+from .forms import FormularioContacto
+
+class Home(CreateView):
+
+    model = Contacto
+
+    form_class = FormularioContacto
+
+    success_url = reverse_lazy('home:home')
+
+    template_name = 'home.html'
+
+    def get_success_url(self):
+        return reverse('home:home')
+
+
